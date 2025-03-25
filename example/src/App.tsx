5de568ed1a38a5287a7a10b5e8e1a141611ed568
1410avi <avinash.soni@grayquest.com>
2025-03-25 19:29:43 +0530

    dev release 1.0.6

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,35 +1,81 @@
 import { useState } from 'react';
-import { SafeAreaView, StyleSheet, Alert, View, Button, Modal } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Button, Modal } from 'react-native';
 import GQPaymentSDK from 'gq_payment_react_native_package';
 
 export default function App() {
   const [GQSDKInititate, setGQSDKInititate] = useState(false);
 
   const clientObject = {
-    auth: {
-      client_id: '<KEY>',
-      client_secret_key: '<KEY>',
-      gq_api_key: '<KEY>'
-    },
     // auth: {
     //   client_id: '<KEY>',
     //   client_secret_key: '<KEY>',
     //   gq_api_key: '<KEY>'
     // },
-    env: "test",
-    student_id: 'std_123429',
-    // customer_number: '8425980928',
-    fee_headers: {
-      "monthly_emi": 15000,
-      "auto_debit": 5000,
-      "direct": 5
-    },
-    // pp_config: {
-    //   slug: "purva-gile"
+
+    // pranit-1
+    // auth: {
+    //   client_id: '<KEY>',
+    //   client_secret_key: '<KEY>',
+    //   gq_api_key: '<KEY>'
     // },
-    customization: {
-      theme_color: "000000"
-    }
+
+    // Edunext
+    // auth: {
+    //   client_id: '<KEY>',
+    //   client_secret_key: '<KEY>',
+    //   gq_api_key: '<KEY>'
+    // },
+
+     // Edunext
+     auth: {
+      client_id: '<KEY>',
+      client_secret_key: '<KEY>',
+      gq_api_key: '<KEY>'
+    },
+
+    // auth: {
+    //   client_id: '<KEY>',
+    //   client_secret_key: '<KEY>',
+    //   gq_api_key: '<KEY>'
+    // },
+
+    // auth: {
+    //   client_id: '<KEY>',
+    //   client_secret_key: '<KEY>',
+    //   gq_api_key: '<KEY>'
+    // },
+    env: "stage",
+    student_id: 'std_121000124',
+    customer_number: '8425900opj',
+    // reference_id: '',
+    // emi_plan_id: "131873",
+    // udf_details: {
+    //   "udf_1":"dsdsdsd"
+    // },
+    // fee_headers: {
+    //   "monthly_emi": 15000,
+    //   "auto_debit": 5000,
+    //   "direct": 5
+    // },
+    fee_headers: {
+      "student_fee": 15000,
+    },
+    pp_config: {
+      // slug: "purva-gile"
+      slug: "edunext"
+      // slug: "arjun-gile"
+    },
+    payment_methods: "['credit_card', 'net_banking']",
+    // fee_headers_split : {
+    //   monthly_emi: {
+    //     "bank_id": "ABC123R",
+    //     "type": "PERCENTAGE",
+    //     "value": "50"
+    //   }
+    // },
+    // customization: {
+    //   theme_color: "000000"
+    // }
   };
 
   const prefillObject = {
@@ -51,20 +97,20 @@ export default function App() {
   // Handle success callback
   const handleSuccess = (data: object) => {
     console.log('Success:', data);
-    Alert.alert('Success', JSON.stringify(data));
+    // Alert.alert('Success', JSON.stringify(data));
   };
 
   // Handle failure callback
   const handleFailure = (error: object) => {
     console.log('Failure:', error);
-    Alert.alert('Failure', JSON.stringify(error));
-    setGQSDKInititate(false)
+    // Alert.alert('Failure', JSON.stringify(error));
+    // setGQSDKInititate(false)
   };
 
   // Handle failure callback
   const handleCancel = (data: object) => {
     console.log('Cancel:', data);
-    Alert.alert('Cancel', JSON.stringify(data));
+    // Alert.alert('Cancel', JSON.stringify(data));
     setGQSDKInititate(false)
   };
 
@@ -80,8 +126,8 @@ export default function App() {
         >
           <View style={styles.container}>
           <GQPaymentSDK 
-            clientObject={clientObject}
-            prefillObject={prefillObject}
+            config={clientObject}
+            prefill={prefillObject}
             onSuccess={handleSuccess}
             onFailed={handleFailure}
             onCancel= {handleCancel}
---
+++ src/GQSecWebView.tsx
@@ -0,0 +1,98 @@
+import React, { useRef } from 'react';
+import { View, StyleSheet, Linking, Platform, StatusBar, SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Environment } from './Environment';
+
+interface GQSecWebViewProps{
+    url: string;
+    onClose: () => void;
+}
+
+const GQSecWebView: React.FC<GQSecWebViewProps> = ({ url, onClose }) => {
+    const webViewRef = useRef<WebView>(null);
+    // console.log("Sec Webview LoadURl: "+url);
+
+  const onNavigationStateChange = (event: any) => {
+    // Detect URL changes
+    // console.log('Current URL:', event.url);
+    const updateURL = event.url;
+    if(updateURL.includes(Environment.getRedirectionURL())){
+        onClose()
+        return false; 
+    }else if (url.startsWith("upi://") || url.startsWith("intent://") || url.startsWith("tez://") || url.startsWith("phonepe://") || url.startsWith("paytmmp://")) {
+      Linking.openURL(url)
+        .catch((err) => {
+          console.error("Failed to open UPI intent:", err);
+          // Alert.alert("Error", "No UPI app found on your device.");
+        });
+      return false; // Prevent WebView from loading the UPI URL
+    }else{
+      return false
+    }
+  };
+
+  const injectedJS = `
+    (function() {
+      // Override window.open to open inside the WebView
+      window.open = function(url) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ newUrl: url }));
+      };
+
+      // Close automatically opened empty tabs (like about:blank)
+      setInterval(() => {
+        if (window.location.href === "about:srcdoc" || window.location.href === "about:blank") {
+          window.close();
+        }
+      }, 500);
+    })();
+  `;
+
+  // console.log('SecWebview Loaded from GQWebView');
+    return(
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+            <WebView
+            ref={webViewRef}
+            originWhitelist={['*']}
+            source={{ uri: url }}
+            javaScriptEnabled={true}
+            setSupportMultipleWindows={true}
+            javaScriptCanOpenWindowsAutomatically={true}
+            onError={(syntheticEvent) => {
+              const { nativeEvent } = syntheticEvent;
+              console.warn('WebView error: ', nativeEvent);
+              // Alert.alert('Error', `WebView failed to load: ${nativeEvent.description}`);
+            }}
+            onShouldStartLoadWithRequest={(request) => {
+                const { url } = request;
+
+                // console.log("secURL: "+url);
+      
+                // ❌ Block about:srcdoc, about:blank, and other invalid URLs
+                if (url.startsWith("about:blank")) {
+                  return false;
+                }
+      
+                // ✅ Allow normal URLs
+                return true;
+              }}
+              injectedJavaScriptBeforeContentLoaded={injectedJS}
+            onNavigationStateChange={onNavigationStateChange}
+            />
+
+        </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, 
+    },
+    webviewContainer: {
+        flex: 1,
+      },
+});
+
+export default GQSecWebView;
